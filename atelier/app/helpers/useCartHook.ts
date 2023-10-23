a61e9188628aa4e876c3c8c90e71e02ef1d928b3
pablosecuen <pablosecuen@gmail.com>
2023-10-23 20:15:59 -0300

    fix for deploy

--- atelier/app/helpers/useCartHook.ts
+++ atelier/app/helpers/useCartHook.ts
@@ -1,5 +1,6 @@
-import { AppDispatch, RootState } from '@/redux/store/store';
+
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../redux/store';
 
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
