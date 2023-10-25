9adcb29e96293fb970af6083b7b457c69bc27163
pablosecuen <pablosecuen@gmail.com>
2023-10-25 14:24:47 -0300

    fix

--- atelier/app/components/categories-banner/index.tsx
+++ atelier/app/components/categories-banner/index.tsx
@@ -8,7 +8,7 @@ import marcas from "@/public/assets/1x/Recurso 5.png";
 
 function CategoriesBanner() {
   const commonClass =
-    "bg-dessshirtbg bg-cover bg-center bg-no-repeat h-[450px] w-[350px] flex justify-start items-end p-2";
+    " bg-cover bg-center bg-no-repeat h-[450px] w-[350px] flex justify-start items-end p-2";
   const imageClass = "flex flex-col justify-center items-center gap-3";
   const containerClass = "w-full px-10 flex text-black md:h-64";
   const itemClass = "flex flex-col items-center justify-center p-8";
@@ -16,28 +16,28 @@ function CategoriesBanner() {
 
   return (
     <>
-      <div className="w-full flex flex-wrap scroll-auto h-96 text-2xl p-10 gap-10 justify-center my-32">
-        <div className={commonClass}>Dress Shirt</div>
-        <div className={commonClass}>Casual Shirt</div>
-        <div className={commonClass}>Athelier</div>
-        <div className={commonClass}>Accesories</div>
+      <div className="w-full flex flex-wrap scroll-auto h-96 text-3xl  p-10 gap-10 justify-center my-32">
+        <div className={`${commonClass} bg-dessshirtbg `}>Dress Shirt</div>
+        <div className={`${commonClass} bg-casualshirtbg`}>Casual Shirt</div>
+        <div className={`${commonClass} bg-athelierbg`}>Athelier</div>
+        <div className={`${commonClass} bg-accesoriesbg`}>Accesories</div>
       </div>
       <div className="w-full px-20 py-10 flex flex-wrap scroll-auto h-96 text-xl  gap-10 justify-around items-center mt-20 text-black ">
-        <div className={imageClass}>
+        <div className={`${imageClass} `}>
           <Image src={flexible} alt="" width={0} height={0} className="w-32 h-24" />
           <span>Secado rapido</span>
         </div>
-        <div className={imageClass}>
+        <div className={`${imageClass} `}>
           {" "}
           <Image src={antiarruga} alt="" width={0} height={0} className="w-24 h-24" />
           <span>Flexible y liger</span>
         </div>
-        <div className={imageClass}>
+        <div className={`${imageClass} `}>
           {" "}
           <Image src={secado} alt="" width={0} height={0} className="w-18 h-24" />
           <span>Antiarrugas</span>
         </div>
-        <div className={imageClass}>
+        <div className={`${imageClass} `}>
           {" "}
           <Image src={cuidado} alt="" width={0} height={0} className="w-24 h-24" />
           <span>Facil Cuidado</span>
