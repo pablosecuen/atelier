b6c651b6d67d03a319770f86aa9d2f871a06311e
pablosecuen <pablosecuen@gmail.com>
2024-03-05 10:32:47 -0300

    .

--- backend/app.ts
+++ backend/app.ts
@@ -16,14 +16,15 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+const webUrl = process.env.WEB_URL || '';
+const adminUrl = process.env.ADMIN_URL || '';
+const deployUrl = process.env.DEPLOY_URL || '';
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["https://andrews-theta.vercel.app", "https://andrews-admin.vercel.app", "https://www.idandrews.com/"],
-   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
+  origin: [webUrl, adminUrl,deployUrl]
 }));
 
 app.use('/api/products', productRoutes);
