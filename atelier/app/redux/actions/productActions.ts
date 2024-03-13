cb20288646dcc9404b5abbab588adc985b4c2b4e
pablosecuen <pablosecuen@gmail.com>
2024-03-13 15:21:55 -0300

    fix

--- atelier/app/redux/actions/productActions.ts
+++ atelier/app/redux/actions/productActions.ts
@@ -3,6 +3,7 @@
 import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
 import { Product } from '@/app/types/general';
 import axiosInstance from '../axiosInstance';
+import { AxiosResponse } from 'axios';
 type FetchProductsArgs = {
   sortKey: string;
   reverse: boolean;
@@ -25,16 +26,34 @@ type FetchProductsByStyleNameArgs = {
   StyleName : string;
 };
 
+const handleAxiosGet = async <T>(
+  url: string,
+  availableForSaleCheck: boolean = false
+): Promise<T[]> => {
+  try {
+    const response: AxiosResponse<T[]> = await axiosInstance.get(url);
+
+    if (availableForSaleCheck) {
+      return response.data.filter((item: any) => item.availableForSale);
+    }
+
+    return response.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+
 export const fetchProductsByStyleName = createAsyncThunk<Product[], FetchProductsByStyleNameArgs>(
   'productActions/fetchProductsByStyleName',
-  async ({ StyleName  }, { rejectWithValue }) => {
+  async ({ StyleName }, { rejectWithValue }) => {
     try {
-      const response = await axiosInstance.get(`/api/products/style/${StyleName }`);
+      const response = await axiosInstance.get(`/api/products/style/${StyleName}`);
       if (response.status !== 200) {
         throw new Error('No se pudo obtener la lista de productos por StyleName.');
       }
       return response.data;
-    } catch (error:any) {
+    } catch (error: any) {
       return rejectWithValue(error.message);
     }
   }
@@ -46,19 +65,15 @@ export const listProducts = createAsyncThunk<Product[], ListProductsArgs | void>
   'productActions/listProducts',
   async (args, { rejectWithValue }) => {
     try {
-      // Realizar la llamada a la API para obtener la lista de productos
-      const response = await fetch(`https://wrong-eggnog-production.up.railway.app/api/products/web`);
-      if (!response.ok) {
-        throw new Error('Error al cargar los productos');
-      }
-      const products = await response.json();
+      const response: AxiosResponse<Product[]> = await axiosInstance.get(`/api/products/web`);
+      const products = response.data;
+
+      const availableProducts = products.filter((product: any) => product.availableForSale);
 
       if (args?.filters?.category) {
-        return products.filter((product: Product) => {
-          return product.category === args.filters.category;
-        });
+        return availableProducts.filter((product: any) => product.category === args.filters.category);
       } else {
-        return products;
+        return availableProducts;
       }
     } catch (error: any) {
       return rejectWithValue(error.message);
@@ -67,17 +82,20 @@ export const listProducts = createAsyncThunk<Product[], ListProductsArgs | void>
 );
 
 
-export const fetchProductsBySort = createAsyncThunk(
+export const fetchProductsBySort = createAsyncThunk<Product[], FetchProductsArgs>(
   'products/fetchProductsBySort',
   async ({ sortKey, reverse, query }: FetchProductsArgs, thunkAPI) => {
-    const url = `/api/products-for-sort?sortKey=${sortKey}&reverse=${reverse}&query=${query || ''}`;
-  
-    const response = await axiosInstance.get(url);
-    if (response.status !== 200) {
+    try {
+      const url = `/api/products-for-sort?sortKey=${sortKey}&reverse=${reverse}&query=${query || ''}`;
+      const response: AxiosResponse<Product[]> = await axiosInstance.get(url);
+
+      // Filtrar los productos que tienen availableForSale como true
+      const availableProducts = response.data.filter((product: Product) => product.availableForSale);
+
+      return availableProducts;
+    } catch (error: any) {
       throw new Error('No se pudo obtener la lista de productos.');
     }
-
-    return response.data;
   }
 );
 
@@ -85,138 +103,86 @@ export const fetchProductsBySort = createAsyncThunk(
 
 // Acción para obtener un producto por ID
 export const getProductById = createAsyncThunk<Product, string>(
-    'productActions/getProductById',
-    async (id: string) => {
-      try {
-        const response = await axiosInstance.get(`/api/product/${id}`);
-        return response.data.product;
-      } catch (error: any) {
-        throw new Error(error.response.data.message);
-      }
-    }
-  );
+  'productActions/getProductById',
+  async (id: string) => {
+    return handleAxiosGet<Product>(`/api/product/${id}`);
+  }
+);
   
 
 // Acción para buscar productos por nombre
 export const searchProductsByName = createAsyncThunk<Product[], string>(
-    'productActions/searchProductsByName',
-    async (name: string) => {
-      try {
-        const response = await axiosInstance.get(`/api/product/search/${name}`);
-        return response.data.products;
-      } catch (error: any) {
-        throw new Error(error.response.data.message);
-      }
-    }
-  );
+  'productActions/searchProductsByName',
+  async (name: string) => {
+    return handleAxiosGet<Product[]>(`/api/product/search/${name}`, true);
+  }
+);
   
 
 export const searchProductByHandle = createAsyncThunk<Product, string>(
-    'productActions/searchProductByHandle',
-    async (handle: string) => {
-      try {
-        const response = await axiosInstance.get(`/api/product/handle/${handle}`);
-        return response.data.product;
-      } catch (error: any) {
-        console.log(error);
-        
-        throw new Error(error.response.data.message);
-      }
-    }
-  );
+  'productActions/searchProductByHandle',
+  async (handle: string) => {
+    return handleAxiosGet<Product>(`/api/product/handle/${handle}`, true);
+  }
+);
 
 
 // Acción para buscar productos por color
 export const searchProductsByColor = createAsyncThunk<Product[], string>(
-    'productActions/searchProductsByColor',
-    async (color: string) => {
-      try {
-        const response = await axiosInstance.get(`/api/product/search/${color}`);
-        return response.data.products;
-      } catch (error: any) {
-        throw new Error(error.response.data.message);
-      }
-    }
-  );
+  'productActions/searchProductsByColor',
+  async (color: string) => {
+    return handleAxiosGet<Product[]>(`/api/product/search/${color}`, true);
+  }
+);
   
  // Acción para buscar productos por rango de precio
 export const searchProductsByPrice = createAsyncThunk<Product[], { minPrice: number; maxPrice: number }>(
-    'productActions/searchProductsByPrice',
-    async ({ minPrice, maxPrice }: { minPrice: number; maxPrice: number }) => {
-      try {
-        const response = await axiosInstance.get(`/api/product/search/${minPrice}/${maxPrice}`);
-        return response.data.products;
-      } catch (error: any) {
-        throw new Error(error.response.data.message);
-      }
-    }
-  );
+  'productActions/searchProductsByPrice',
+  async ({ minPrice, maxPrice }: { minPrice: number; maxPrice: number }) => {
+    return handleAxiosGet<Product[]>(`/api/product/search/${minPrice}/${maxPrice}`, true);
+  }
+);
   
 
 // Acción para buscar productos por descripción
 export const searchProductsByDescription = createAsyncThunk<Product[], string>(
-    'productActions/searchProductsByDescription',
-    async (description: string) => {
-      try {
-        const response = await axiosInstance.get(`/api/product/search/${description}`);
-        return response.data.products;
-      } catch (error: any) {
-        throw new Error(error.response.data.message);
-      }
-    }
-  );
+  'productActions/searchProductsByDescription',
+  async (description: string) => {
+    return handleAxiosGet<Product[]>(`/api/product/search/${description}`, true);
+  }
+);
   
 
 // Acción para buscar productos por estado de eliminación (isDeleted)
 export const searchProductsByIsDeleted = createAsyncThunk<Product[], boolean>(
-    'productActions/searchProductsByIsDeleted',
-    async (isDeleted: boolean) => {
-      try {
-        const response = await axiosInstance.get(`/api/product/search/${isDeleted}`);
-        return response.data.products;
-      } catch (error: any) {
-        throw new Error(error.response.data.message);
-      }
-    }
-  );
+  'productActions/searchProductsByIsDeleted',
+  async (isDeleted: boolean) => {
+    return handleAxiosGet<Product[]>(`/api/product/search/${isDeleted}`, true);
+  }
+);
   
 
 // Acción para buscar productos en venta (inSale)
 export const searchProductsInSale = createAsyncThunk<Product[], boolean>(
-    'productActions/searchProductsInSale',
-    async (inSale: boolean) => {
-      try {
-        const response = await axiosInstance.get(`/api/product/search/${inSale}`);
-        return response.data.products;
-      } catch (error: any) {
-        throw new Error(error.response.data.message);
-      }    }
-  );
+  'productActions/searchProductsInSale',
+  async (inSale: boolean) => {
+    return handleAxiosGet<Product[]>(`/api/product/search/${inSale}`, true);
+  }
+);
   
-export const searchProductByHandleAndId = createAsyncThunk(
-    'productActions/searchProductByHandleAndId',
-    async ({ handle, id }: { handle: string; id: string }) => {
-      try {
-        const response = await axiosInstance.get(`/api/product/search/${handle}/${id}`);
-        return response.data.product; 
-      } catch (error: any) {
-        throw new Error(error.response.data.message);
-      }
-    }
-  );
+export const searchProductByHandleAndId = createAsyncThunk<Product, { handle: string; id: string }>(
+  'productActions/searchProductByHandleAndId',
+  async ({ handle, id }: { handle: string; id: string }) => {
+    return handleAxiosGet<Product>(`/api/product/search/${handle}/${id}`, true);
+  }
+);
 
-export const findProductsByCategory = createAsyncThunk(
-    "productActions/findProductsByCategory",
-    async (category: string) => {
-      try {
-        const response = await axiosInstance.get(`/api/product/category/${category}`); 
-        const data = await response.data.products;
-        return data;
-      } catch (error: any) {
-        throw new Error(error.response.data.message);
-      }
-    }
-  );
+export const findProductsByCategory = createAsyncThunk<Product[], string>(
+  'productActions/findProductsByCategory',
+  async (DeptName: string) => {
+    return handleAxiosGet<Product[]>(`/api/product/category/${DeptName}`, true);
+  }
+);
 
 // Acción para actualizar un producto por su ID
 export const updateProductById = createAsyncThunk<Product, { id: string; updatedProduct: Product }>(
